0b00459a42eff26540327f620029631e6accd1f7
Luxe17 <hanson1717@live.com>
2023-02-28 14:54:01 -0700

    Added models

---
+++ models/category.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
+
+const CategorySchema = new Schema({
+    name: { 
+        type: String, 
+        required: true,
+        enum: ["CPU", "Motherboard", "Memory", "Storage", "GPU", "Case", "Power Supply"]
+    },
+    description: { type: String}
+})
+
+CategorySchema.virtual("url").get(function(){
+    return `/category/${this._id}`
+})
+
+module.exports = mongoose.model("Category", CategorySchema)
---
+++ models/part.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose')
+
+const Schema = mongoose.Schema;
+
+const PartSchema = new Schema({
+    name: { type: String, required: true },
+    description: { type: String },
+    category: { type: Schema.Types.ObjectId, ref: "Category", required: true },
+    price: { type: Number, required: true },
+    stock: { type: Number, required: true }
+})
+
+PartSchema.virtual("url").get(function(){
+    return `/part/${this._id}`
+})
+
+module.exports = mongoose.model("Part", PartSchema)
