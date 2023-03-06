40b7c90624f4612e8c6351590374fe75efc76aa9
Luxe17 <hanson1717@live.com>
2023-03-06 16:25:22 -0700

    Added route for part_detail

--- routes/categories.js
+++ routes/categories.js
@@ -7,4 +7,6 @@ router.get('/', category_controller.category_list)
 
 router.get('/category/:id', category_controller.part_list)
 
+router.get('/part/:id', category_controller.part_detail)
+
 module.exports = router;
